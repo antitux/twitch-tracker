bc96b15a470957c48ab7ef6c39d62987f7f8646d
Antitux <me@antitux.dev>
2026-06-24 10:20:37 -0700

    fix firefox_desktop_restart.sh script

--- scripts/firefox_desktop_restart.sh
+++ scripts/firefox_desktop_restart.sh
@@ -4,4 +4,4 @@ pkill firefox
 sleep 3
 
 # 3. Force execution of your exact autostart file by its full path
-DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/1000/bus" /usr/bin/dex /home/user/.config/autostart/firefox.desktop
+DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/$(id -u)/bus" /usr/bin/dex ~/.config/autostart/firefox.desktop
